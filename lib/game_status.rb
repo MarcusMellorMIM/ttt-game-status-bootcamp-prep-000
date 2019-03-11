d5e3ad030e9ac1ced8c5c3dd95269392b4864a6b
Marcus Mellor <marcus@mellor.com>
2019-03-11 04:12:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,38 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6] ]
+  
+  def won?( board )
+    
+    WIN_COMBINATIONS.each do | win_comb |
+   
+      result=''
+    
+      win_comb.each do | position |
+        
+        result="#{result}#{board[position]}"
+      end
+      
+      if result == 'XXX' || result == 'OOO'
+        return win_comb
+      end
+   
+    end   
+      
+    false 
+      
+  end
+        
+  def full?( board )
+    
+  
+  end
